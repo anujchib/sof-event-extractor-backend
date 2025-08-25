1b0dc043a49fd7706eb69d326d102cdbaef96a3f
anujchib <anujchibofficial0003@gmail.com>
2025-08-25 18:25:36 +0530

    cors

--- config/aws.js
+++ config/aws.js
@@ -10,7 +10,7 @@ dotenv.config();
     region:process.env.AWS_REGION,
     credentials:{
         accessKeyId:process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY_ID
+        secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY
     }
 
 });
@@ -19,7 +19,7 @@ dotenv.config();
     region:process.env.AWS_REGION,
     credentials:{
         accessKeyId:process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY_ID
+        secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY
     }
 });
 
